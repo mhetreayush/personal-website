26e1d31bce744a5a2730812a39984b7506595c83
Ayush Mhetre <mhetreayush1719@gmail.com>
2024-03-26 17:11:55 +0530

    💄 Fix favicon rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,25 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-export const metadata = {
+import { Metadata } from "next";
+export const metadata: Metadata = {
   title: "Ayush Mhetre",
   description: "Ayush Mhetre's personal website",
   openGraph: {
     images: "/opengraph-image.jpeg",
   },
+  keywords: [
+    "Ayush Mhetre",
+    "Personal Website",
+    "Portfolio",
+    "Resume",
+    "Blog",
+    "Projects",
+    "Contact",
+    "About",
+    "Frontend",
+    "Backend",
+    "Web developer",
+  ],
 };
 import "react-vertical-timeline-component/style.min.css";
 export default function RootLayout({
@@ -19,6 +33,7 @@ export default function RootLayout({
         name="google-site-verification"
         content="LAFLPJO73XDxXz1ZA7Oa8g_g3xdfcL24i9sEhlt9G6o"
       />
+      <link rel="icon" href="/favicon.ico" />
       <meta name="theme-color" content="#F5EFE7" />
       <body>{children}</body>
       <Analytics />
