3f562f8228e0f2b20df1b96febd45e096d89c077
Ayush Mhetre <mhetreayush1719@gmail.com>
2024-04-04 09:35:37 +0530

    ✨ Vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Ayush Mhetre",
@@ -37,6 +38,7 @@ export default function RootLayout({
       <meta name="theme-color" content="#F5EFE7" />
       <body>{children}</body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
