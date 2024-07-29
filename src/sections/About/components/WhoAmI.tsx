62e79d88c60452a9347c46e9dd7ddd33c1e3f225
mhetreayush <mhetreayush1719@gmail.com>
2024-07-29 14:37:03 +0530

    Update about

--- src/sections/About/components/WhoAmI.tsx
+++ src/sections/About/components/WhoAmI.tsx
@@ -1,16 +1,11 @@
 export const WhoAmI = () => (
   <div>
-    <h1 className="font-semibold underline underline-offset-8 mb-1">
-      Who am I?
-    </h1>
+    <h1 className="font-semibold underline underline-offset-8 mb-1">Who am I?</h1>
     <p>
-      I am a 3rd year 👨🏼‍🎓 student pursuing B.Tech in 💻 Computer Science and
-      Engineering from 🏫 Vellore Institute of Technology, Vellore. I am a
-      passionate 👨🏼‍💻 developer and a tech enthusiast. I love to ✨ explore new
-      technologies and leverage them to solve real-life problems. 🕸 I am into
-      Web Development for almost 2 years and currently working on my Data
-      Structures and Algorithms. I also keep an understanding of web
-      fundamentals, security issues and SEO.
+      I am a 4th (final) year 👨🏼‍🎓 student pursuing B.Tech in 💻 Computer Science and Engineering from 🏫 Vellore
+      Institute of Technology, Vellore. I am a passionate 👨🏼‍💻 developer and a tech enthusiast. I love to ✨ explore new
+      technologies and leverage them to solve real-life problems. 🕸 I have been doing web development for 2.5 years. I
+      also keep an understanding of web fundamentals, security issues and SEO.
     </p>
   </div>
 );
