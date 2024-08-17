132e379ad86157f17da8d3ce6dcfaf882a0c46bc
mhetreayush <mhetreayush1719@gmail.com>
2024-08-17 12:45:54 +0530

    Update blogs

--- src/assets/data/blogs.tsx
+++ src/assets/data/blogs.tsx
@@ -1,41 +1,41 @@
-import { BlogType } from "@/lib/types";
+import { BlogType } from '@/lib/types';
 
 const blogs_data: BlogType[] = [
   {
     hidden: true,
-    link: "https://medium.com/@mhetreayush1719/are-you-sure-about-your-accordions-898e1b65b42",
-    title: "You might be building accordions wrong!",
-    folder: "accordions",
+    link: 'https://medium.com/@mhetreayush1719/are-you-sure-about-your-accordions-898e1b65b42',
+    title: 'You might be building accordions wrong!',
+    folder: 'accordions',
     about: (
       <ul>
         <li>What are accordions?</li>
         <li>How it can affect your SEO.</li>
         <li>
           <code>
-            {"<summary>"} & {"<details>"}
-          </code>{" "}
+            {'<summary>'} & {'<details>'}
+          </code>{' '}
           tag in HTML
         </li>
       </ul>
     ),
-    date: "23-03-2023",
+    date: '23-03-2023',
     tags: [
-      "ReactJS",
-      "HTML_tags",
-      "HTML",
-      "JavaScript",
-      "Accordions",
-      "Web_crawlers",
-      "Conditional_rendering",
-      "MDN",
-      "Summary",
-      "Details",
+      'ReactJS',
+      'HTML_tags',
+      'HTML',
+      'JavaScript',
+      'Accordions',
+      'Web_crawlers',
+      'Conditional_rendering',
+      'MDN',
+      'Summary',
+      'Details',
     ],
   },
   {
-    link: "https://medium.com/@mhetreayush1719/did-you-know-about-discriminated-unions-in-typescript-da8bccd7e69f",
-    folder: "discriminated_unions",
-    title: "Did you know about discriminated unions in TypeScript?",
+    link: 'https://medium.com/@mhetreayush1719/did-you-know-about-discriminated-unions-in-typescript-da8bccd7e69f',
+    folder: 'discriminated_unions',
+    title: 'Did you know about discriminated unions in TypeScript?',
     about: (
       <ul>
         <li>What are discriminated unions?</li>
@@ -43,15 +43,36 @@ const blogs_data: BlogType[] = [
         <li>What are the benefits of using them?</li>
       </ul>
     ),
-    date: "29-12-2023",
-    tags: [
-      "TypeScript",
-      "JavaScript",
-      "Discriminated_unions",
-      "Union_types",
-      "Clean_code",
-      "Type_safety",
-    ],
+    date: '29-12-2023',
+    tags: ['TypeScript', 'JavaScript', 'Discriminated_unions', 'Union_types', 'Clean_code', 'Type_safety'],
+  },
+  {
+    link: 'https://medium.com/@mhetreayush1719/top-javascript-questions-asked-in-an-interview-part-1-2e4d6ee63127',
+    folder: 'js_solutions',
+    title: 'Top javascript questions asked in an interview. — Part 1',
+    about: (
+      <ul>
+        <li>Top JS questions asked in interview.</li>
+        <li>Solutions to the questions.</li>
+        <li>Github Repository with code and tests</li>
+      </ul>
+    ),
+    date: '17-08-2024',
+    tags: ['TypeScript', 'JavaScript', 'Frontend', 'Interview', 'Interview_Questions'],
+  },
+  {
+    link: 'https://medium.com/@mhetreayush1719/top-javascript-questions-asked-in-an-interview-part-2-eb8c572a0959',
+    folder: 'js_solutions',
+    title: 'Top javascript questions asked in an interview. — Part 2',
+    about: (
+      <ul>
+        <li>Top JS questions asked in interview.</li>
+        <li>Solutions to the questions.</li>
+        <li>Github Repository with code and tests</li>
+      </ul>
+    ),
+    date: '17-08-2024',
+    tags: ['TypeScript', 'JavaScript', 'Frontend', 'Interview', 'Interview_Questions'],
   },
 ];
 
--- src/sections/Blogs/components/BlogCard.tsx
+++ src/sections/Blogs/components/BlogCard.tsx
@@ -1,37 +1,27 @@
-import Image, { StaticImageData } from "next/image";
-import { AnimatedButton } from "@/components/AnimatedButton";
-import { ExternalLinkSVG } from "@/components/ExternalLinkSVG";
-import { Tags } from "@/components/Tags/Tags";
-import { slugify } from "@/lib/utils/slugify";
-import { BlogCardProps } from "../types";
-import { PostedAgo } from "./PostedAgo";
-import accordionsCover from "../../../../public/blog-assets/accordions/cover.png";
-import discriminatedUnions from "../../../../public/blog-assets/discriminated_unions/cover.png";
+import Image, { StaticImageData } from 'next/image';
+import { AnimatedButton } from '@/components/AnimatedButton';
+import { ExternalLinkSVG } from '@/components/ExternalLinkSVG';
+import { Tags } from '@/components/Tags/Tags';
+import { slugify } from '@/lib/utils/slugify';
+import { BlogCardProps } from '../types';
+import { PostedAgo } from './PostedAgo';
+import accordionsCover from '../../../../public/blog-assets/accordions/cover.png';
+import discriminatedUnions from '../../../../public/blog-assets/discriminated_unions/cover.png';
+import jsSolutions from '../../../../public/blog-assets/js_solutions/cover.png';
 const blogImages: {
   [key: string]: StaticImageData;
 } = {
   accordions: accordionsCover,
   discriminated_unions: discriminatedUnions,
+  js_solutions: jsSolutions,
 };
 
-const BlogCard = ({
-  title,
-  link,
-  folder,
-  about,
-  tags,
-  date,
-  parentID,
-}: BlogCardProps) => {
+const BlogCard = ({ title, link, folder, about, tags, date, parentID }: BlogCardProps) => {
   return (
-    <AnimatedButton rounded="rounded-md" className={{ child: "bg-white " }}>
+    <AnimatedButton rounded="rounded-md" className={{ child: 'bg-white ' }}>
       <div className="flex flex-col lg:flex-row gap-3 items-center p-4">
         <div className="border border-black rounded-md h-fit md:min-w-[500px] max-h-[300px] max-w-[500px] overflow-clip ">
-          <a
-            href={link}
-            target="_blank"
-            aria-label={"Blog: " + slugify(title, "-")}
-          >
+          <a href={link} target="_blank" aria-label={'Blog: ' + slugify(title, '-')}>
             <Image
               src={blogImages[folder]}
               alt={title}
@@ -45,22 +35,17 @@ const BlogCard = ({
           <div className="flex flex-col gap-y-3">
             <div className="flex  w-full gap-x-3">
               <div>
-                {" "}
+                {' '}
                 <p className="md:text-2xl font-semibold ">{title}</p>
               </div>
               <div>
-                <ExternalLinkSVG
-                  link={link}
-                  name={"Blog: " + slugify(title, "-")}
-                />
+                <ExternalLinkSVG link={link} name={'Blog: ' + slugify(title, '-')} />
               </div>
             </div>
             <PostedAgo date={date} />
             {about}
           </div>
-          <div className="flex w-full gap-4 flex-wrap">
-            {tags && <Tags tags={tags} parentID={parentID} />}
-          </div>
+          <div className="flex w-full gap-4 flex-wrap">{tags && <Tags tags={tags} parentID={parentID} />}</div>
         </div>
       </div>
     </AnimatedButton>
