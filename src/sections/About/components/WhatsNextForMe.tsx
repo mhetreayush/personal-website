d82db79888e70d7cbc7eedc3160a6f6f8d7406b3
mhetreayush <mhetreayush1719@gmail.com>
2024-10-30 23:11:41 +0530

    update whats next for me

--- src/sections/About/components/WhatsNextForMe.tsx
+++ src/sections/About/components/WhatsNextForMe.tsx
@@ -5,10 +5,14 @@ export const WhatsNextForMe = () => (
     <SectionHeading heading="What's next for me?" />
     <p>
       As I approach graduation, I&apos;m currently working as an Engineering Explorer (Intern) in the Founder&apos;s
-      Office at <strong>Superjoin</strong>, where I&apos;m building a cutting-edge product with some of the most
-      talented engineers in the field. My focus is on full stack web development, and I&apos;m continuously sharpening
-      my skills in Data Structures and Algorithms. I also have a strong interest in system design and am deepening my
-      knowledge of design patterns, architectures, and scalability to take on more complex challenges.
+      Office at{' '}
+      <strong>
+        <a href="https://superjoin.ai" className="" target="_blank">
+          Superjoin
+        </a>
+      </strong>
+      , where I&apos;m building products to solve data problems for B2B SaaS with some of the most talented engineers in
+      the field.
     </p>
   </div>
 );
