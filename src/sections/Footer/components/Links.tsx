90b725b16f10a0e57c5ac04c9c97e94f8c4a264a
mhetreayush <mhetreayush1719@gmail.com>
2024-11-02 10:48:40 +0530

    fix socials

--- src/sections/Footer/components/Links.tsx
+++ src/sections/Footer/components/Links.tsx
@@ -1,38 +1,36 @@
-import { slugify } from "@/lib/utils/slugify";
-import Image from "next/image";
-import { LinkType } from "../types";
+import { slugify } from '@/lib/utils/slugify';
+import Image from 'next/image';
+import { LinkType } from '../types';
 
 const links: LinkType[] = [
   {
-    label: "Github",
-    url: "https://github.com/mhetreayush",
+    label: 'Github',
+    url: 'https://github.com/mhetreayush',
   },
   {
-    label: "StackOverflow",
-    url: "https://stackoverflow.com/users/22811320/ayush-mhetre",
+    label: 'StackOverflow',
+    url: 'https://stackoverflow.com/users/22811320/ayush-mhetre',
   },
   {
-    label: "Medium",
-    url: "https://medium.com/@mhetreayush1719",
+    label: 'Medium',
+    url: 'https://medium.com/@mhetreayush1719',
   },
   {
-    label: "LinkedIn",
-    url: "https://www.linkedin.com/in/ayush-mhetre",
+    label: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/ayush-mhetre',
   },
   {
-    label: "Phone",
-    url: "tel:+917020860691",
+    label: 'Phone',
+    url: 'tel:+917020860691',
+    hidden: true,
   },
 ];
 export const Links = () => {
-  return links.map((link, idx) => (
-    <a key={idx} href={link.url} target="_blank" aria-label={link.label}>
-      <Image
-        src={`/icons/${slugify(link.label)}.svg`}
-        width={36}
-        height={36}
-        alt={link.label}
-      />
-    </a>
-  ));
+  return links.map((link, idx) =>
+    link.hidden ? null : (
+      <a key={idx} href={link.url} target="_blank" aria-label={link.label}>
+        <Image src={`/icons/${slugify(link.label)}.svg`} width={36} height={36} alt={link.label} />
+      </a>
+    )
+  );
 };
--- src/sections/Footer/types.ts
+++ src/sections/Footer/types.ts
@@ -1,4 +1,5 @@
 export type LinkType = {
   label: string;
   url: string;
+  hidden?: boolean;
 };
